e535067e27cdc502e95d115e293ddd550dc73e8c
hari <markapuramhari@gmail.com>
2025-03-04 11:32:26 +0530

    first commit

---
+++ tests/kickStart.spec.ts
@@ -0,0 +1,14 @@
+import {test} from '@playwright/test'
+
+test('Kick Start with Playwright Test',()=>{
+
+    console.log('My First Test')
+})
+
+test('My 2nd Test Script',()=>{
+
+
+    console.log('2nd Test Script')
+
+})
+
